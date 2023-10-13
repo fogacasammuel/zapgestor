be19e1dcb3b117e66bdf49d77ef46f1b69212fb3
gestordeveloper <developer@gestor.in>
2023-10-13 19:19:25 -0300

    organização nas rotas e melhoria do codigo

--- source/app.js
+++ source/app.js
@@ -24,7 +24,86 @@ venom
   });
 
 function start(client) {
-  app.post("/sendImageGroup", async (req, res) => {
+  //users
+  app.post("/text", async (req, res) => {
+    const body = req.body;
+
+    client.sendText(body.number, body.content).catch((erro) => {
+      return res
+        .status(500)
+        .json({ error: 500, message: "Error when sending", erro: erro });
+    });
+
+    return res.status(200).json();
+  });
+
+  //groups
+  app.get("/groups", async (req, res) => {
+    let data = [];
+
+    try {
+      const groups = await client.getAllChats().then((chats) => {
+        return chats.find((chat) => {
+          if(chat.isGroup) {
+            data.push({
+              group: chat.id._serialized,
+              name: chat.name
+            });
+          }
+        });
+      });
+
+      return res.status(200).json({ data });
+    } catch (e) {
+      return res
+        .status(404)
+        .json({ error: 404, message: e.message });
+    }
+  });
+
+  app.post("/group/text", async (req, res) => {
+    const body = req.body;
+
+    const group = await findGroupByName(client, body.group_name);
+    if (!group) {
+      return res
+        .status(404)
+        .json({ error: 404, message: "Not found group name!" });
+    }
+
+    client.sendText(group.id._serialized, body.content).catch((erro) => {
+      return res
+        .status(500)
+        .json({ error: 500, message: "Error when sending", erro: erro });
+    });
+
+    return res.status(200).json();
+  });
+
+  app.post("/group/link", async (req, res) => {
+    const body = req.body;
+
+    const group = await findGroupByName(client, body.group_name);
+    if (!group) {
+      return res
+        .status(404)
+        .json({ error: 404, message: "Not found group name!" });
+    }
+
+    client.sendLinkPreview(
+        group.id._serialized,
+        body.link, 
+        body.content
+      ).catch((erro) => {
+      return res
+        .status(500)
+        .json({ error: 500, message: "Error when sending", erro: erro });
+    });
+
+    return res.status(200).json();
+  });
+
+  app.post("/group/image", async (req, res) => {
     const body = req.body;
 
     const group = await findGroupByName(client, body.group_name);
@@ -43,21 +122,59 @@ function start(client) {
       )
       .catch((erro) => {
         return res
-          .status(500)
-          .json({ error: 500, message: "Error when sending", erro: erro });
+          .status(404)
+          .json({ error: 404, message: "Error when sending", erro: erro });
       });
 
     return res.status(200).json();
   });
 
-  app.post("/sendText", async (req, res) => {
+  app.post("/group/file", async (req, res) => {
     const body = req.body;
 
-    client.sendText(body.number, body.content).catch((erro) => {
+    const group = await findGroupByName(client, body.group_name);
+    if (!group) {
       return res
-        .status(500)
-        .json({ error: 500, message: "Error when sending", erro: erro });
-    });
+        .status(404)
+        .json({ error: 404, message: "Not found group name!" });
+    }
+
+    client
+      .sendFile(
+        group.id._serialized,
+        body.file_path,
+        "file-send",
+        body.content
+      )
+      .catch((erro) => {
+        return res
+          .status(404)
+          .json({ error: 404, message: "Error when sending", erro: erro });
+      });
+
+    return res.status(200).json();
+  });
+
+  app.post("/group/voice", async (req, res) => {
+    const body = req.body;
+
+    const group = await findGroupByName(client, body.group_name);
+    if (!group) {
+      return res
+        .status(404)
+        .json({ error: 404, message: "Not found group name!" });
+    }
+
+    client
+      .sendVoice(
+        group.id._serialized,
+        body.voice_path
+      )
+      .catch((erro) => {
+        return res
+          .status(404)
+          .json({ error: 404, message: "Error when sending", erro: erro });
+      });
 
     return res.status(200).json();
   });
